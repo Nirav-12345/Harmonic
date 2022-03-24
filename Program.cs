06e28ae1b2748f64494d9863bab59aef9c55f05e
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-24 10:47:52 +0530

    adding harmonic class and prgram files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Harmonic_term;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Class1 b = new Class1();
+        b.Harmonic();
+    }
+        
+
+}
